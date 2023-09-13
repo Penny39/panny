d8b6b8b8eeb07895fef8d5f64efad478364237a9
liuxiyayuan <2048991179@qq.com>
2023-09-13 20:53:29 +0800

    第四次

--- push.sh
+++ push.sh
@@ -2,5 +2,6 @@ git config --global user.name 'penny39'
 git config --global user.email '2098334896@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m $自动推送
+git commit -m $1
 git push
+
